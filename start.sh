f99e8b79afe32f294c53b8761a13012dce468487
Jimmy Gitonga <jimmy@afroshok.com>
2020-10-28 16:47:58 +0300

    CHORE: Repair cookie consent, banner carousel and redirect to login if not authenticated on clicking `Submit Your Memoranda`.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-# docker-compose -f production.yml build
+docker-compose -f production.yml build --compress --force-rm --pull --parallel
 docker-compose -f production.yml up -d django
  
 while [ ! "curl $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' django):5000 | grep doctype" ]; do
@@ -7,8 +7,10 @@ sleep 2
 done
 
 docker-compose -f production.yml up -d nginx
+
 docker-compose -f production.yml up certbot
 docker exec nginx sed -i -r 's/#?;#//g' /etc/nginx/conf.d/default.conf
 docker exec nginx service nginx restart
+docker-compose -f production.yml up -d documentserver
 
 docker rm -f certbot
