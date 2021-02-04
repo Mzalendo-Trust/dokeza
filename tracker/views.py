1ffd65d4572c139aeaeaa016da0ac3ba60108736
Jimmy Gitonga <jimmy@afroshok.com>
2021-02-04 17:20:44 +0300

    FIX: Make changes to the tracker UI in line with the rest of the site.

--- tracker/views.py
+++ tracker/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'tracker/index.html', {
-        'upath': request.path.split("/")[-1],
+        'upath': request.path.split("/")[-1], 'page': 'tracker',
     })
