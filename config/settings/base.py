a63beb80f15b858cf778b9b1c77dc303f67eb282
Jimmy Gitonga <jimmy@ara.co.ke>
2021-06-07 16:44:34 +0300

    FIX: Proxy production to staging. Allow OnlyOffice to download documents.

--- config/settings/base.py
+++ config/settings/base.py
@@ -21,10 +21,13 @@ if READ_DOT_ENV_FILE:
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG", False)
+# DEBUG = env.bool("DJANGO_DEBUG", False)
+DEBUG = True
+
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
+
 # In Windows, this must be set to your system time zone.
 TIME_ZONE = "Africa/Nairobi"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
